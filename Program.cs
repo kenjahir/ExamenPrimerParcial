91d707de5cd816f4472532e937472be2fafc3976
COMPUTER <COMPUTER@KEVINJAHIR>
2022-06-03 22:12:46 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExamenPrimerParcial
+{
+    class Program
+    {
+        //Console.WriteLine("hi");
+        static void Main(String[] args);
+        {
+          estudiantes_primaria estudiantes_primaria = new estudiantes_primaria(100, "Gina Alejanadra", "Zambrano Loor", "UEQ", 1234567890);
+          estudiante_primaria.Imprimir_datos();
+
+    }
+}
---
+++ estudiante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExamenPrimerParcial
+{
+    class estudiante
+    {
+        string nombre { get; set; }
+        string apellido { get; set; }
+        string institucion { get; set; }
+        int identificacion { get; set; }
+
+        public class alumnos (string nombre, string apellido, string institucion, int identificacion)
+            {
+             
+                 this.nombre =nombre;
+                 this.apellido = apellido;
+                 this.identicacion = identificacion;
+
+            }
+    }
+}
